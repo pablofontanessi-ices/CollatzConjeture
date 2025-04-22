9f746bc68fdf3c256ac62c979f93d46529e146b8
pablofontanessi-ices <pablo.fontanessi@ices.edu.ar>
2025-04-22 17:53:44 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzConjeture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? Num;
+            do
+            {
+                Console.WriteLine("Ingrese un numero positivo");
+                Num = int.Parse(Console.ReadLine());
+            }
+            while (Num < 1);
+            int pasos = 0;
+            while (Num !=1)
+            {
+                if (Num % 2 == 0)//Par
+                {
+                    Num = Num / 2;
+                }
+                else //Impar
+                {
+                    Num = (Num * 3) + 1;
+                }
+                pasos++;
+            }
+            Console.WriteLine(pasos);
+        }
+    }
+}
